3c263a35e24ff49630417e2dea59eae769b85553
RushikeshWaghamare <rrwaghamare798@gmail.com>
2025-08-22 19:18:50 +0530

    Add project files.

---
+++ APICRUD.Web/Controllers/EmployeeController.cs
@@ -0,0 +1,89 @@
+﻿using APICRUD.Web.Data;
+using APICRUD.Web.Models;
+using APICRUD.Web.Models.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICRUD.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        public readonly ApplicationDbContext _dbContext;
+        public EmployeeController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {
+            var employees = _dbContext.Employees.ToList();
+            return Ok(employees);
+        }
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        public IActionResult GetEmployeeById(Guid id)
+        {
+            var employee = _dbContext.Employees.Find(id);
+            if(employee == null)
+            {
+                return NotFound();
+            }
+            return Ok(employee);
+        }
+
+        [HttpPost]
+        public IActionResult AddEmployee(AddEmployeeDto addEmployeeDto)
+        {
+            var employeeEntity = new Employee
+            {
+                Name = addEmployeeDto.Name,
+                Email = addEmployeeDto.Email,
+                Phone = addEmployeeDto.Phone,
+                Salary = addEmployeeDto.Salary,
+            };
+
+            _dbContext.Employees.Add(employeeEntity);
+            _dbContext.SaveChanges();
+
+            return Ok(employeeEntity);
+        }
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public IActionResult UpdateEmployee(Guid id, UpdateEmployeeDto updateEmployeeDto)
+        {
+            var employee = _dbContext.Employees.Find(id);
+            if(employee == null)
+            {
+                return NotFound();
+            }
+
+            employee.Name = updateEmployeeDto.Name;
+            employee.Email = updateEmployeeDto.Email;
+            employee.Phone = updateEmployeeDto.Phone;
+            employee.Salary = updateEmployeeDto.Salary;
+
+            _dbContext.SaveChanges();
+            return Ok(employee);
+        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public IActionResult DeleteEmployee(Guid id)
+        {
+            var employee = _dbContext.Employees.Find(id);
+            if(employee == null)
+            {
+                return NotFound();
+            }
+            _dbContext.Remove(employee);
+            _dbContext.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ APICRUD.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using APICRUD.Web.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICRUD.Web.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {            
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
